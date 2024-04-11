01882df89c948e77b4dd5bf8bfe885b9bae1e08f
Mark Gardner <themarkgardner@gmail.com>
2024-04-11 14:24:08 -0500

    added basic geolocation function

--- playdate/app/components/Header/Header.tsx
+++ playdate/app/components/Header/Header.tsx
@@ -1,7 +1,39 @@
+'use client'
+
 import Image from "next/image";
 import Weather from "../Weather/Weather";
+import { useState } from "react";
+
+export interface WeatherProps {
+    lat: number;
+    long: number;
+    error?: string;
+}
 
 export default function Header() {
+    const [lat, setLat] = useState<number>(0)
+    const [long, setLong] = useState<number>(0)
+    const [error, setError] = useState<string | undefined>();
+    const locationSuccess = async (position: GeolocationPosition) => {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+    }
+
+    const locationError = async () => {
+        console.error('error in getting location')
+        setError("Error in getting location")
+    }
+
+    //Check for geolocation in browser
+    if ('geolocation' in navigator) {
+        console.log('geolocation is available');
+        // continue with using geolocation 
+        navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+    } else {
+        console.log(' geolocation IS NOT available ');
+        setError("Geolocation is not available")
+    };
+
     return (
         <>
             <header className="w-full h-8 pt-2 pr-2 pl-2">
@@ -9,11 +41,11 @@ export default function Header() {
                     src="/logos/playdate_logo.webp"
                     alt='Playdate logo'
                     width='100'
-                    height='100'
+                    height='20'
                     className=''>
                 </Image>
             </header>
-            <Weather />
+            <Weather lat={lat} long={long} error={error} />
         </>
     )
 }
--- playdate/app/components/Weather/Weather.tsx
+++ playdate/app/components/Weather/Weather.tsx
@@ -1,7 +1,20 @@
-export default function Weather() {
+import { useState } from "react"
+import { WeatherProps } from "../Header/Header"
+
+export default function Weather(props: WeatherProps) {
+    console.log(props)
+
     return (
         <section className='w-full pl-2 pr-2'>
             <p>Weather</p>
+            {props.error ?
+                <div>Weather info not available</div>
+                :
+                <>
+                    <div>{props.lat}</div>
+                    <div>{props.long}</div>
+                </>
+            }
         </section>
     )
 }
