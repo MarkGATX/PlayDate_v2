b14553d140f6494f003a2107e95ce1fa959336d3
Mark Gardner <themarkgardner@gmail.com>
2024-04-13 00:59:30 -0500

    switched use client to weather component from header

--- playdate/app/components/Header/Header.tsx
+++ playdate/app/components/Header/Header.tsx
@@ -1,38 +1,7 @@
-'use client'
-
 import Image from "next/image";
 import Weather from "../Weather/Weather";
-import { useState } from "react";
-
-// export interface WeatherProps {
-//     lat: number;
-//     long: number;
-//     error?: string;
-// }
 
 export default function Header() {
-    // const [lat, setLat] = useState<number>(0)
-    // const [long, setLong] = useState<number>(0)
-    // const [error, setError] = useState<string | undefined>();
-    // const locationSuccess = async (position: GeolocationPosition) => {
-    //     setLat(position.coords.latitude)
-    //     setLong(position.coords.longitude)
-    // }
-
-    // const locationError = async () => {
-    //     console.error('error in getting location')
-    //     setError("Error in getting location")
-    // }
-
-    // //Check for geolocation in browser
-    // if ('geolocation' in navigator) {
-    //     console.log('geolocation is available');
-    //     // continue with using geolocation 
-    //     navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-    // } else {
-    //     console.log(' geolocation IS NOT available ');
-    //     setError("Geolocation is not available")
-    // };
 
     return (
         <>
--- playdate/app/components/Weather/Weather.tsx
+++ playdate/app/components/Weather/Weather.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useLocationContext } from "@/utils/location/LocationContext"
 
 export default function Weather() {
