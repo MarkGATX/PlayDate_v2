2c86de7d92276fb5492fe72d0e31d1f65a7ce4ec
Mark Gardner <themarkgardner@gmail.com>
2024-04-23 11:53:42 -0500

    used new package to render mapbox and more easily map through markers later

--- playdate/app/components/MapContainer/MapContainer.tsx
+++ playdate/app/components/MapContainer/MapContainer.tsx
@@ -3,48 +3,18 @@ import { LocationContext } from "@/utils/location/LocationContext";
 import { getActivitiesFromWeather } from "@/utils/map/mapData";
 import { WeatherContext } from "@/utils/weather/WeatherContext";
 import { useContext, useEffect, useRef } from "react";
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-// import Map from 'react-map-gl'
-// import styles from './MapContainer.module.scss'
+import Map from 'react-map-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+import styles from './MapContainer.module.scss'
+
 
 export default function MapContainer() {
     const currentLocation = useContext(LocationContext)
     const currentWeather = useContext(WeatherContext)
     const currentWeatherCode = currentWeather.weatherData?.current_icon
-    const mapContainer = useRef<any>(null)
-    const map = useRef<mapboxgl.Map | any>(null);
-
-    console.log(currentWeatherCode)
 
     useEffect(() => {
-        if (process.env.NEXT_PUBLIC_MAPBOX_API) {
-            mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API
-            // if (map.current) return; // initialize map once
-            if (currentLocation && mapContainer.current) {
-                console.log(currentLocation)
-                map.current = new mapboxgl.Map({
-                    container: mapContainer.current, // container ID
-                    style: 'mapbox://styles/mapbox/streets-v12',
-                    center: [currentLocation.longitude, currentLocation.latitude], // starting position [lng, lat]
-                    zoom: 11 // starting zoom
-                });
-                console.log(map.current)
-            }
-            // if (map.current) {
-            // Add navigation control (the +/- zoom buttons)
-            // map.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-            // map.current.on('move', () => {
-            //     setLng(map.getCenter().lng.toFixed(4));
-            //     setLat(map.getCenter().lat.toFixed(4));
-            //     setZoom(map.getZoom().toFixed(2));
-            // });
-            // }
-
-            // Clean up on unmount
-
-        }
         const fetchMapData = async () => {
             if (currentLocation && currentWeather && currentWeather.weatherData) {
                 const mapUrls = await getActivitiesFromWeather(currentLocation.latitude, currentLocation.longitude, currentWeatherCode);
@@ -60,9 +30,16 @@ export default function MapContainer() {
             <div >
                 Longitude: {currentLocation.longitude} | Latitude: {currentLocation.latitude}
             </div>
-            <div id='map' className="map-container" ref={mapContainer}>
-                <div ref={map} ></div>
-            </div>
+            <Map
+                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API}
+                initialViewState={{
+                    longitude: currentLocation.longitude,
+                    latitude: currentLocation.latitude,
+                    zoom: 14
+                }}
+                style={{ width: '100dvw', height: '40dvh' }}
+                mapStyle="mapbox://styles/mapbox/streets-v12"
+            />
         </>
     )
 }
