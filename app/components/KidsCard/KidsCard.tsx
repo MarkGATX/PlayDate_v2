64be65a4e1449b3d5b487a3f6ad78a8c1e37cada
Mark Gardner <themarkgardner@gmail.com>
2024-06-13 13:33:23 -0500

    tried to add smooth animations when switching between edits and readonly on kids card but having issues with the animation. need to revisit later after more function complete

--- app/components/KidsCard/KidsCard.tsx
+++ app/components/KidsCard/KidsCard.tsx
@@ -1,9 +1,12 @@
 import { KidsType } from "@/utils/types/userTypeDefinitions";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SubmitButton from "../SubmitButton/SubmitButton";
 import { EditKid } from "@/utils/actions/actions";
 import { useFormStatus } from "react-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 
 export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentUser: string }) {
     const [editKidInfo, setEditKidInfo] = useState<boolean>(false)
@@ -11,7 +14,13 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
     const [kidLastName, setKidLastName] = useState<string>(kid.last_name)
     const [kidBirthday, setKidBirthday] = useState<string | undefined>(kid.birthday)
     const [kidFirstNameOnly, setKidFirstNameOnly] = useState<boolean>(kid.first_name_only)
+    const [kidFirstNameBlank, setKidFirstNameBlank] = useState<boolean>(false)
+    const [kidLastNameBlank, setKidLastNameBlank] = useState<boolean>(false)
     const { pending } = useFormStatus()
+    const firstNameErrorMessageRef = useRef<HTMLParagraphElement | null>(null);
+    const lastNameErrorMessageRef = useRef<HTMLParagraphElement | null>(null);
+    const editFormRef = useRef<HTMLFormElement | null>(null)
+    const readOnlyKidContentRef = useRef<HTMLDivElement | null>(null)
 
     //use js bind method to include kid id in submitted form with form action
     // const editKidWithId = EditKid.bind(null,kid.id )
@@ -20,12 +29,10 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
         setEditKidInfo(previousValue => !previousValue)
     }
 
-    
-    
     const saveKidEdits = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
         const editedKidData: KidsType = {
-       
+
             id: kid.id,
             first_name: kidFirstName.trim(),
             last_name: kidLastName.trim(),
@@ -35,10 +42,61 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
         }
         console.log(editedKidData)
         if (!editedKidData.first_name || editedKidData.first_name === '') {
+            setKidFirstNameBlank(true)
+            if (firstNameErrorMessageRef.current) {
+                //may need to add to useGSAP to make sure cleanup happens. use context safe. to add to useGSAP hook https://gsap.com/resources/React/
+                gsap.to(firstNameErrorMessageRef.current,
+                    {
+                        autoAlpha: 1,
+                        maxHeight: '200px',
+                        duration: 2,
+                        ease: 'power1.out',
+                        textContent: `First name can't be blank`
+                    }
+                )
+            }
             return
+        } else if (editedKidData.first_name) {
+            setKidFirstNameBlank(false)
+            if (firstNameErrorMessageRef.current) {
+                gsap.to(firstNameErrorMessageRef.current,
+                    {
+                        autoAlpha: 0,
+                        maxHeight: 0,
+                        duration: 0.7,
+                        ease: 'power1.out',
+                        textContent: ``
+                    }
+                )
+            }
         }
         if (!editedKidData.last_name || editedKidData.last_name === '') {
+            setKidLastNameBlank(true)
+            if (lastNameErrorMessageRef.current) {
+                gsap.to(lastNameErrorMessageRef.current,
+                    {
+                        autoAlpha: 1,
+                        maxHeight: '200px',
+                        duration: 2,
+                        ease: 'power1.out',
+                        textContent: `Last name can't be blank`
+                    }
+                )
+            }
             return
+        } else if (editedKidData.last_name) {
+            setKidLastNameBlank(false)
+            if (lastNameErrorMessageRef.current) {
+                gsap.to(lastNameErrorMessageRef.current,
+                    {
+                        autoAlpha: 0,
+                        maxHeight: 0,
+                        duration: 0.7,
+                        ease: 'power1.out',
+                        textContent: ``
+                    }
+                )
+            }
         }
 
         try {
@@ -46,7 +104,7 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
             if (updateResponse) {
                 setEditKidInfo(previousValue => !previousValue)
                 console.log(updateResponse)
-                
+
             } else {
                 console.log('something else')
                 console.log(updateResponse)
@@ -79,10 +137,88 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
         setKidFirstNameOnly(kid.first_name_only)
         setKidFirstNameOnly(kid.first_name_only)
         setEditKidInfo(false)
+        setKidFirstNameBlank(false)
+        setKidLastNameBlank(false)
     }
 
+    // useEffect(() => {
+    //     if (editKidInfo) {
+    //         console.log('editing')
+    //         gsap.from(editFormRef.current, {
+    //             duration: 2,
+    //             ease: 'power3.inOut',
+    //             opacity: 1,
+    //             // className: 'hidden', // Remove hidden class after animation
+    //         });
+    //         gsap.to(readOnlyKidContentRef.current, {
+    //             duration: 2,
+    //             ease: 'power1.out',
+    //             opacity: 0,
+    //             // onComplete: () => readOnlyKidContentRef.current.classList.add('hidden'), // Hide visually after animation
+    //         });
+    //     } else {
+    //         gsap.to(editFormRef.current, {
+    //             duration: 2,
+    //             ease: 'power1.out',
+    //             opacity: 0,
+    //             // className: 'hidden', // Add hidden class before animation
+    //         });
+    //         gsap.from(readOnlyKidContentRef.current, {
+    //             duration: 2,
+    //             ease: 'power3.inOut',
+    //             opacity: 1,
+    //             // onComplete: () => readOnlyKidContentRef.current.classList.remove('hidden'), // Show visually after animation
+    //         });
+    //     }
+
+    // }, [editKidInfo]);
+
+    // useGSAP(() => {
+    //     if (editKidInfo) {
+    //         console.log('editing')
+    //         gsap.from(editFormRef.current, {
+    //             translateX:'0px',
+    //             duration: 2,
+    //             ease: 'power3.inOut',
+    //             // opacity: 1,
+    //             // display: 'block', // Remove hidden class after animation
+    //             // autoAlpha:1
+    //         });
+    //         gsap.to(readOnlyKidContentRef.current, {
+    //             duration: 2,
+    //             translateX:'200%',
+    //             ease: 'power1.out',
+    //             // autoAlpha:0
+    //             // opacity: 0,
+    //             // display:'none'
+    //             // onComplete: () => readOnlyKidContentRef.current.classList.add('hidden'), // Hide visually after animation
+    //         });
+    //     } 
+    //     if (!editKidInfo) {
+    //         gsap.to(editFormRef.current, {
+    //             duration: 2,
+    //             ease: 'power1.out',
+    //             translateX: '200%',
+    //             // opacity: 0,
+    //             // display: 'none', // Add hidden class before animation
+    //             // autoAlpha:0
+    //         });
+    //         gsap.from(readOnlyKidContentRef.current, {
+    //             duration: 2,
+    //             ease: 'power3.inOut',
+    //             // autoAlpha:1,
+    //             translateX: '0px'
+    //             // opacity: 1,
+    //             // display:'block'
+    //             // onComplete: () => readOnlyKidContentRef.current.classList.remove('hidden'), // Show visually after animation
+    //         });
+    //     }
+
+    // }, [editKidInfo]);
+
+
     return (
-        <div key={kid.id} className='singleKid flex flex-col bg-inputBG rounded-xl p-2 gap-4'>
+        <div key={kid.id} className='singleKid flex flex-col bg-inputBG rounded-xl p-2 gap-4 overflow-hidden'>
             <div className='flex justify-between items-start gap-4 w-full '>
                 <div id='kidProfilePicContainer' className='flex flex-col w-1/4 items-center justify-start'>
                     <div id='kidProfilePic' className='relative w-16 h-16 max-h-20 rounded-full border-appBlue border-2 bg-appBG overflow-hidden'>
@@ -90,13 +226,16 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
                     </div>
                     <button className='px-1 w-90 text-xs cursor-pointer py-1 mt-2 bg-appGold hover:bg-appBlue active:bg-appGold active:shadow-activeButton active:text-appBlue hover:text-appGold border-2 border-appBlue rounded-lg transform ease-in-out duration-300 disabled:opacity-50 disabled:pointer-events-none' >Edit pic</button>
                 </div>
-                <div className='w-3/4 flex flex-col gap-y-1'>
+                <div className='w-3/4 flex flex-col gap-y-1 transition-all'>
                     {editKidInfo ?
-                        <form >
+                        <form ref={editFormRef} >
                             {/* <form action={editKidWithId}> */}
-                            <input id='kidsFirstNameInput' name='first_name' type="text" placeholder="First name" required={true} className='rounded border-2 border-appBlue p-1 text-sm ml-2 w-2/3' value={kidFirstName} onChange={(event) => { setKidFirstName(event.target.value) }} ></input> <input id='kidsLastNameInput' name='last_name' type="text" placeholder="Last name" required={true} className='rounded border-2 border-appBlue p-1 text-sm ml-2 w-2/3 my-2' value={kidLastName} onChange={(event) => { setKidLastName(event.target.value) }}></input>
-                            <div className='block w-full text-xs'>
-                                <input type='checkbox' id='showLastNameToggle' className='mr-2' checked={kidFirstNameOnly} onChange={(event) => {setKidFirstNameOnly(event.target.checked)}}></input><label htmlFor="showLastNameToggle">First name only</label>
+                            <p id='firstNameErrorMessage' ref={firstNameErrorMessageRef} className='opacity-0 max-h-0 text-xs text-red-700 font-bold'></p>
+                            <input id='kidsFirstNameInput' name='first_name' type="text" placeholder="First name" required={true} className={`rounded border-2 p-1 text-sm w-2/3 mb-2 ${kidFirstNameBlank ? 'border-red-700' : 'border-appBlue'}`} value={kidFirstName} onChange={(event) => { setKidFirstName(event.target.value) }} ></input>
+                            <p id='lastNameErrorMessage' ref={lastNameErrorMessageRef} className='opacity-0 max-h-0 text-xs text-red-700 font-bold'></p>
+                            <input id='kidsLastNameInput' name='last_name' type="text" placeholder="Last name" required={true} className={`rounded border-2 p-1 text-sm w-2/3 mb-2 ${kidLastNameBlank ? 'border-red-500' : 'border-appBlue'}`} value={kidLastName} onChange={(event) => { setKidLastName(event.target.value) }}></input>
+                            <div className='flex w-full text-xs mb-2'>
+                                <input type='checkbox' id='showLastNameToggle' className='mr-2' checked={kidFirstNameOnly} onChange={(event) => { setKidFirstNameOnly(event.target.checked) }}></input><label htmlFor="showLastNameToggle">First name only</label>
                             </div>
                             <div className='block w-full text-xs'>
                                 <label htmlFor="birthday" className='mr-2'>Birthday</label><input id='kidsBirthdayInput' name='birthday' type="date" className='rounded border-2 border-appBlue p-1 text-sm ml-2 w-2/3' value={kidBirthday} onChange={(event) => { setKidBirthday(event.target.value) }}></input>
@@ -109,8 +248,9 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
                                 <button className='px-1 w-90 text-xs cursor-pointer py-1 mt-2 bg-appGold hover:bg-appBlue active:bg-appGold active:shadow-activeButton active:text-appBlue hover:text-appGold border-2 border-appBlue rounded-lg transform ease-in-out duration-300 disabled:opacity-50 disabled:pointer-events-none' onClick={cancelKidEdits}>Cancel</button>
                             </div>
                         </form>
-                        :
-                        <>
+                         : 
+                        <div ref={readOnlyKidContentRef}>
+
                             <p className='w-full'>{kidFirstName} {kidLastName}</p>
                             {kid.primary_caregiver === currentUser
                                 ?
@@ -126,8 +266,8 @@ export default function KidsCard({ kid, currentUser }: { kid: KidsType, currentU
                             <div className='block w-full text-xs'>
                                 <button className='px-1 w-90 text-xs cursor-pointer py-1 mt-2 bg-appGold hover:bg-appBlue active:bg-appGold active:shadow-activeButton active:text-appBlue hover:text-appGold border-2 border-appBlue rounded-lg transform ease-in-out duration-300 disabled:opacity-50 disabled:pointer-events-none' onClick={handleEditKid}>Edit Kid Info</button>
                             </div>
-                        </>
-                    }
+                        </div>
+                    }  
 
                 </div>
             </div>
