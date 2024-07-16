2845cf5e325a52ddcc0709993665b859b012f0aa
Mark Gardner <themarkgardner@gmail.com>
2024-07-16 16:13:36 -0500

    found unescaped characters

--- app/playdates/add/not-found.tsx
+++ app/playdates/add/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <main className="flex flex-col items-center justify-start overflow-y-hidden">
             <h2 className='font-bold text-lg p-4 bg-appBlue w-full text-appBG text-center'>Page not found</h2>
             <div className='p-4'>
-                <p className='mb-4'>We couldn't find any information to create a new Playdate. </p>
+                <p className='mb-4'>{`We couldn't find any information to create a new Playdate.`} </p>
                 <p className='mb-4'>Try searching again on the home page and select a new location.</p>
                 <Link href="/">
                     <button className='px-1 w-90 text-xs cursor-pointer py-1 mt-2 bg-appGold hover:bg-appBlue active:bg-appGold active:shadow-activeButton active:text-appBlue hover:text-appGold border-2 border-appBlue rounded-lg transform ease-in-out duration-300 disabled:opacity-50 disabled:pointer-events-none' >Back to Home</button>
