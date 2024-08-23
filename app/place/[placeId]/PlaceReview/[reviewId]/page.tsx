6bd7a77dc4df67cb18ba0eb299ee35734d66d69e
Mark Gardner <themarkgardner@gmail.com>
2024-08-23 12:00:46 -0500

    added ability to see review in separate page

--- app/place/[placeId]/PlaceReview/[reviewId]/page.tsx
+++ app/place/[placeId]/PlaceReview/[reviewId]/page.tsx
@@ -5,7 +5,7 @@ import { fetchPlaceDetails } from "@/utils/map/placeDetailsAPI";
 import supabaseClient from "@/utils/supabase/client";
 import { placeReviewType, placesDataType } from "@/utils/types/placeTypeDefinitions";
 import Image from "next/image";
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { EffectFade, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'quill/dist/quill.snow.css';
@@ -14,13 +14,45 @@ import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import '@/utils/SwiperCustom/swiperCustom.scss'
+import Quill from "quill";
 
 // export default function PlaceReview({ params }: { params: { reviewId: string } }) {
-    export default function PlaceReview({ params }: { params: { reviewId:string} }) {
-    console.log(params)
-
+export default function PlaceReview({ params }: { params: { reviewId: string } }) {
+    const { reviewId } = params
     const [currentReview, setCurrentReview] = useState<placeReviewType | null>();
     const [currentPlace, setCurrentPlace] = useState<placesDataType>()
+    const [quill, setQuill] = useState<Quill | null>(null);
+    const [openReviewEditor, setOpenReviewEditor] = useState<boolean>(false)
+    const editorRef = useRef<HTMLDivElement>(null);
+    const toolbarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (editorRef.current && toolbarRef.current && !quill) {
+            const quillInstance = new Quill(editorRef.current, {
+                theme: 'snow',
+                modules: {
+                    toolbar: toolbarRef.current
+                }
+            });
+            setQuill(quillInstance);
+        }
+    }, [quill, currentReview]);
+
+    // Separate useEffect for setting contents
+    useEffect(() => {
+        if (quill && currentReview?.reviewer_notes) {
+            quill.setContents(currentReview.reviewer_notes);
+        }
+    }, [quill, currentReview]);
+
+    useEffect(() => {
+        if (quill) {
+            quill.enable(openReviewEditor);
+            if (toolbarRef.current) {
+                toolbarRef.current.style.display = openReviewEditor ? 'block' : 'none';
+            }
+        }
+    }, [openReviewEditor, quill]);
 
     useEffect(() => {
         const getPlaceReview = async () => {
@@ -28,7 +60,7 @@ import '@/utils/SwiperCustom/swiperCustom.scss'
                 const { data: reviewData, error: reviewDataError } = await supabaseClient
                     .from('Location_Reviews')
                     .select('*')
-                    .eq('id', params.reviewId);
+                    .eq('id', reviewId);
                 if (reviewDataError) {
                     throw reviewDataError;
                 }
@@ -60,11 +92,10 @@ import '@/utils/SwiperCustom/swiperCustom.scss'
                 console.error(error)
             }
         }
-
         getPlaceReview();
     }, [params.reviewId])
 
-    console.log(currentPlace)
+    console.log(currentReview)
     return (
         <main >
             <div id='placePicContainer' className='flex h-[250px] w-full'>
@@ -129,34 +160,64 @@ import '@/utils/SwiperCustom/swiperCustom.scss'
                     }
                 </div>
                 <fieldset className='flex flex-wrap mt-4 w-full'>
-                    {amenityList.map((amenity, index) => {
-                        const transformedAmenity = amenity.toLowerCase().replace(/\s+/g, '_');
-                        // const isChecked = currentReview && currentReview[transformedAmenity] === true;
-                        return (
-                            <div key={index} className='w-1/2 flex justify-start'>
-                                <input type='checkbox' id={transformedAmenity} ></input>
-                                <label htmlFor={transformedAmenity} className='pl-2 text-sm'>{amenity}</label>
-                            </div>
-                        )
-                    })}
+                    {currentReview ? (
+                        (() => {
+                            //use rest to isolate the available amenities.
+                            const { stars, reviewer_notes, reviewer_id, google_place_id, id, ...amenities } = currentReview;
+                            // iterate over the new amenities object and 
+                            return Object.entries(amenities).map(([key, value], index) => {
+                                const amenity = key.replace(/_/g, ' ');
+                                return (
+                                    <div key={index} className='w-1/2 flex justify-start'>
+                                        <input
+                                            type='checkbox'
+                                            id={key}
+                                            checked={value}
+                                            readOnly
+                                            //false inputs still allowed checks so added onclick to prevent
+                                           onClick={(event)=> event.preventDefault()} 
+                                            aria-readonly="true"
+                                        />
+                                        <label htmlFor={key} className='pl-2 text-sm'>{amenity}</label>
+                                    </div>
+                                );
+                            });
+                        })()
+                    ) : (
+                        amenityList.map((amenity, index) => {
+                            const transformedAmenity = amenity.toLowerCase().replace(/\s+/g, '_');
+                            return (
+                                <div key={index} className='w-1/2 flex justify-start'>
+                                    <input type='checkbox' id={transformedAmenity} />
+                                    <label htmlFor={transformedAmenity} className='pl-2 text-sm'>{amenity}</label>
+                                </div>
+                            );
+                        })
+                    )}
                 </fieldset>
-                {/* <div className={`quillEditorContainer w-full min-h-48 rounded flex flex-col justify-center border-appBlue border-2 mt-4`}>
-                    <div ref={toolbarRef}>
-                        <span className="ql-formats">
-                            <button className="ql-bold"></button>
-                            <button className="ql-italic"></button>
-                            <button className="ql-underline"></button>
-                        </span>
-                        <span className="ql-formats">
-                            <button className="ql-list" value="ordered"></button>
-                            <button className="ql-list" value="bullet"></button>
-                        </span>
-                        <span className="ql-formats">
-                            <button className="ql-clean"></button>
-                        </span>
+                {currentReview && currentReview.reviewer_notes
+                    ?
+                    <div className={`quillEditorContainer w-full min-h-48 rounded flex flex-col justify-center border-appBlue border-2 mt-4`}>
+                        <div ref={toolbarRef}>
+                            <span className="ql-formats">
+                                <button className="ql-bold"></button>
+                                <button className="ql-italic"></button>
+                                <button className="ql-underline"></button>
+                            </span>
+                            <span className="ql-formats">
+                                <button className="ql-list" value="ordered"></button>
+                                <button className="ql-list" value="bullet"></button>
+                            </span>
+                            <span className="ql-formats">
+                                <button className="ql-clean"></button>
+                            </span>
+                        </div>
+                        <div ref={editorRef} ></div>
                     </div>
-                    <div ref={editorRef} ></div>
-                </div> */}
+                    :
+                    null
+                }
+
 
                 {/* <div id='reviewSaveButtonContainer' className='flex w-full justify-center mt-4'>
                     <button className='px-4 min-w-48 text-sm cursor-pointer py-2 bg-appGold hover:bg-appBlue hover:text-appGold border-2 border-appBlue rounded-xl transform ease-in-out duration-300 active:bg-appGold active:shadow-activeButton active:text-appBlue disabled:opacity-50  disabled:pointer-events-none' onPointerDown={(event) => handleSaveReview(event)}>Save Your Review</button>
