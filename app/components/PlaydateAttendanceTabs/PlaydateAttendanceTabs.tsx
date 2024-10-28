91036c828d4653e72a76880e14776fd896e61f0a
Mark Gardner <themarkgardner@gmail.com>
2024-10-27 23:43:20 -0500

    added hover on attendance tabs for better ux

--- app/components/PlaydateAttendanceTabs/PlaydateAttendanceTabs.tsx
+++ app/components/PlaydateAttendanceTabs/PlaydateAttendanceTabs.tsx
@@ -119,14 +119,14 @@ export default function PlaydateAttendanceTabs({
       <h3 className="font-bold"> Responses:</h3>
       <div id="tabs" className="flex w-full justify-between gap-2">
         <div
-          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "yes" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBG bg-inputBG"}`}
+          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "yes" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBG bg-inputBG"} hover:font-bold`}
           onClick={() => handleClick("yes")}
         >
           {yesResponse.length > 0 ? (
             <>
               <p>Yes</p>
               <div
-                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "yes" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"}`}
+                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "yes" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"} hover:font-bold`}
               >
                 {yesResponse.length}
               </div>
@@ -137,14 +137,14 @@ export default function PlaydateAttendanceTabs({
         </div>
 
         <div
-          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "no" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBG bg-inputBG"}`}
+          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "no" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBG bg-inputBG"} hover:font-bold`}
           onClick={() => handleClick("no")}
         >
           {noResponse.length > 0 ? (
             <>
               <p>No</p>
               <div
-                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "no" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"}`}
+                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "no" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"} hover:font-bold`}
               >
                 {noResponse.length}
               </div>
@@ -154,14 +154,14 @@ export default function PlaydateAttendanceTabs({
           )}
         </div>
         <div
-          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "maybe" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"}`}
+          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "maybe" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"} hover:font-bold`}
           onClick={() => handleClick("maybe")}
         >
           {maybeResponse.length > 0 ? (
             <>
               <p>Maybe</p>
               <div
-                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "maybe" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"}`}
+                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "maybe" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"} hover:font-bold`}
               >
                 {maybeResponse.length}
               </div>
@@ -173,14 +173,14 @@ export default function PlaydateAttendanceTabs({
           )}
         </div>
         <div
-          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "invited" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBlue bg-inputBG"}`}
+          className={`relative flex w-1/4 cursor-pointer items-center justify-center rounded-xl text-center text-xs ${selectedTab === "invited" ? "border-2 border-appBlue bg-appGold" : "border-1 border-appBlue bg-inputBG"} hover:font-bold`}
           onClick={() => handleClick("invited")}
         >
           {invitedResponse.length > 0 ? (
             <>
               <p>Invited</p>
               <div
-                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "invited" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"}`}
+                className={`absolute -right-2 -top-2 inline-block w-auto min-w-4 rounded-full ${selectedTab === "invited" ? "border-2 border-appBlue bg-appGold" : "border-2 border-appBG bg-inputBG"} hover:font-bold`}
               >
                 {invitedResponse.length}
               </div>
